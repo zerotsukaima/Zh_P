56da9a9e6816df690e5e86ec9e03dc4601f7980a
zerotsukaima <de-mambo@yandex.ru>
2021-03-16 19:06:53 +0300

    изменила обращение к библиотеке

--- DZ2.py
+++ DZ2.py
@@ -1,9 +1,10 @@
-import math
+from math import pi
 R1 = float(input("Введите R1 "))
 R2 = float(input("Введите R2 при условии R1>R2 "))
-S1 = (math.pi * (R1 ** 2))
-S2 = (math.pi * (R2 ** 2))
-S3 = (math.pi * ((R1 ** 2) - (R2 ** 2)))
+S1 = (pi * (R1 ** 2))
+S2 = (pi * (R2 ** 2))
+S3 = (pi * ((R1 ** 2) - (R2 ** 2)))
 print(S1)
 print(S2)
 print(S3)
+
