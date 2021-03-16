1e5ee4e94acdd65661051cb46fc466413c3a6aa7
zerotsukaima <de-mambo@yandex.ru>
2021-03-16 18:44:33 +0300

    ввела библиотеку для Пи

--- DZ2.py
+++ DZ2.py
@@ -1,9 +1,9 @@
+import math
 R1 = float(input("Введите R1 "))
 R2 = float(input("Введите R2 при условии R1>R2 "))
-P = float(3.14)
-S1 = (P * (R1 ** 2))
-S2 = (P * (R2 ** 2))
-S3 = (P * ((R1 ** 2) - (R2 ** 2)))
+S1 = (math.pi * (R1 ** 2))
+S2 = (math.pi * (R2 ** 2))
+S3 = (math.pi * ((R1 ** 2) - (R2 ** 2)))
 print(S1)
 print(S2)
 print(S3)
