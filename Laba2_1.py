fce96845198cb4d20aff30916eb024777ef0b831
zerotsukaima <de-mambo@yandex.ru>
2021-03-17 18:11:25 +0300

    добавила корректировку в расчете программы

--- Laba2_1.py
+++ Laba2_1.py
@@ -1,4 +1,9 @@
-s = input("Vvedite frazu ")
-while len(s) < 10
+s = input("Введите фразу ")
+while len(s) < 10:
     s = input("Vvedite frazu ")
+else:
+    print("Первые 4 символа", s[:4])
+    print("Последние 4 символа", s[-4:])
+    print('Символ посередине', s[len(s) // 2])
+    print("Символы с 3-го по 8-й", s[2:8])
 
