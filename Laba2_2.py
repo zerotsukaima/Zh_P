bb07139c2e3c982b33de5e014dde12904ce6ab8a
zerotsukaima <de-mambo@yandex.ru>
2021-03-18 19:38:39 +0300

    попытка взять библиотеку децимал

--- Laba2_2.py
+++ Laba2_2.py
@@ -1,9 +1,13 @@
 import math
+from decimal import Decimal
 x = float(input("Vvedite znachecnie X "))
 n = int(input("Vvedite chislo shagov "))
 step = 1
-y = 1.0
+y = 1
 while step <= n:
-    y += float((-1) ** step) * (x ** step) / math.factorial(step)
+    y += ((-1) ** step) * (x ** step) / math.factorial(step)
     step = step + 1
 print(y)
+#завести отдельно переменную Числитель и потом применять ее в примере, чтобы Числитель вызывать и домножать на себя
+#со знаменателем тоже самое, завести переменную Знаменатель и выполнять ее домножение
+#в итоговой формуле будет y += Числитель/Знаменатель
