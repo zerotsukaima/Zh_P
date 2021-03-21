8e6f4c0a20410720e5ff28e706d1b57f0b267c0b
zhanna <de-mambo@yandex.ru>
2021-03-21 16:16:07 +0300

    Решение задачи про сумму и ср.ариф.

---
+++ сумма и срариф.py
@@ -0,0 +1,8 @@
+sum = 0
+for i in range(1,7):
+    a = input("Введите число ")
+    for i in a:
+        sum = sum + int(i)
+        srArifm = sum / 6
+print("Сумма= ", sum)
+print("Ср.арифм. = ", srArifm)
